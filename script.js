6688adaccb42033beee5a232cd826ec433828ead
Cheryl Rendone <chmills1990@yahoo.com>
2019-07-14 20:53:12 -0400

    June 14th commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-// function blink_text() {
-//     $('.warning').fadeOut(5);
-//     $('.warning').fadeIn(650);
-// }
+function blink_text() {
+    $('.warning').fadeOut(5);
+    $('.warning').fadeIn(650);
+}
 
-// setInterval(blink_text);
+setInterval(blink_text);
